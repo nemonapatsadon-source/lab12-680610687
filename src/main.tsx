1d9981cb4e10a3b2987c0dc918c55dee2c1480ab
Nemo <nemonapatsadon@gmail.com>
2026-09-05 17:04:45 +0700

    ....

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import Sidebar from "./components/Sidebar";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 import TodolistPage from "./pages/TodolistPage";
-import "bootstrap/dist/css/bootstrap.min.css";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <TodolistPage />
+    <div className="d-flex">
+      {" "}
+      <Sidebar username="napatsadon" type="admin" />
+      <div className="flex-grow-1">
+        <Header />
+        <TodolistPage />
+        <Footer year="2026" fullName="นภัสดล จำปา" studentId="680610687" />
+      </div>
+    </div>
   </React.StrictMode>,
 );
